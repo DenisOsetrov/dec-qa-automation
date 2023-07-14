f1ae5df6cf44314db7aa0ec29e2df66ff0ee0bfc
VladyslavKoshman <vko@primexm.com>
2023-07-14 20:40:37 +0300

    lesson6 framework, logger

--- src/main/java/base/pages/LoginPage.java
+++ src/main/java/base/pages/LoginPage.java
@@ -1,26 +1,23 @@
 package base.pages;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
+import base.core.PageTools;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
+public class LoginPage extends PageTools {
 
-public class LoginPage {
-
-    private final SelenideElement loginInput = $(By.xpath("//input[@placeholder='Username']"));
-    private final SelenideElement passwordInput = $(By.xpath("//input[@placeholder='Password']"));
-    private final SelenideElement loginButton = $(By.xpath("//input[@type='submit']"));
+    private final By loginInput = By.xpath("//input[@placeholder='Username']");
+    private final By passwordInput = By.xpath("//input[@placeholder='Password']");
+    private final By loginButton = By.xpath("//input[@type='submit']");
 
     public void typeUserName(String username) {
-        loginInput.shouldBe(Condition.visible).append(username);
+        type(username, loginInput);
     }
 
     public void typePassword(String password) {
-        passwordInput.shouldBe(Condition.visible).append(password);
+        type(password, passwordInput);
     }
     public void clickLoginButton() {
-        loginButton.shouldBe(Condition.enabled).click();
+        click(loginButton);
     }
 
 
--- src/test/java/lessons/innerPackage/lesson5/PageObjectTest.java
+++ src/test/java/lessons/innerPackage/lesson5/PageObjectTest.java
@@ -2,19 +2,10 @@ package lessons.innerPackage.lesson5;
 
 import base.config.BaseTest;
 import base.pages.BurgerMenuPage;
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import static base.Pages.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class PageObjectTest extends BaseTest {
@@ -22,54 +13,67 @@ public class PageObjectTest extends BaseTest {
     @Test
     public void test() {
 
-        loginPage().typeUserName("standard_user");
-        loginPage().typePassword("secret_sauce");
-        loginPage().clickLoginButton();
-
-        mainPage().wainUntilMainPageLoad();
-
-        System.out.println(mainPage().getMainPageItemsText());
-
-        mainPage().clickBurgerMenu();
-
-        Selenide.sleep(1000);
-
-        List<String> expectedTextsFromBurgerMenu = new ArrayList<>();
-        expectedTextsFromBurgerMenu.add("All Items");
-        expectedTextsFromBurgerMenu.add("About");
-        expectedTextsFromBurgerMenu.add("Logout");
-        expectedTextsFromBurgerMenu.add("Reset App State");
-
-        Assert.assertEquals(expectedTextsFromBurgerMenu, burgerMenuPage.getBurgerMenuItemsTexts());
-
-        burgerMenuPage.clickCloseBurgerMenuButton();
-
-        footerPage().clickSocialFacebook();
-
-        Selenide.switchTo().window(1);
+        Selenide.sleep(2000);
 
 
-        Selenide.sleep(5000);
+        Selenide.switchTo().frame("frame1");
 
-        $(By.xpath("//div[@aria-label='Close']")).shouldBe(Condition.visible).click();
+        System.out.println($(By.xpath("//h1")).text());
 
-        Assert.assertTrue($(By.xpath("//h1[text()='Sauce Labs']")).is(Condition.visible));
+        Selenide.switchTo().defaultContent();
 
-        WebDriver webDriver = WebDriverRunner.getWebDriver(); // extract web driver in selenide
+        Selenide.switchTo().frame("frame2");
 
-        Set<String> windowHandles = webDriver.getWindowHandles();
+        System.out.println($(By.xpath("//h1")).text());
 
-        Assert.assertEquals(windowHandles.size(), 2);
-
-        Selenide.closeWindow();
-
-        Selenide.switchTo().window(0);
-
-        mainPage().getMainPageItems().get(2).click();
-
-        detailItemPage().waitDetailPageImg();
-
-        System.out.println(detailItemPage().getDetailItemName() + " - " + detailItemPage().getDetailItemPrice());
+//        loginPage().typeUserName("standard_user");
+//        loginPage().typePassword("secret_sauce");
+//        loginPage().clickLoginButton();
+//
+//        mainPage().wainUntilMainPageLoad();
+//
+//        System.out.println(mainPage().getMainPageItemsText());
+//
+//        mainPage().clickBurgerMenu();
+//
+//        Selenide.sleep(1000);
+//
+//        List<String> expectedTextsFromBurgerMenu = new ArrayList<>();
+//        expectedTextsFromBurgerMenu.add("All Items");
+//        expectedTextsFromBurgerMenu.add("About");
+//        expectedTextsFromBurgerMenu.add("Logout");
+//        expectedTextsFromBurgerMenu.add("Reset App State");
+//
+//        Assert.assertEquals(expectedTextsFromBurgerMenu, burgerMenuPage.getBurgerMenuItemsTexts());
+//
+//        burgerMenuPage.clickCloseBurgerMenuButton();
+//
+//        footerPage().clickSocialFacebook();
+//
+//        Selenide.switchTo().window(1);
+//
+//
+//        Selenide.sleep(5000);
+//
+//        $(By.xpath("//div[@aria-label='Close']")).shouldBe(Condition.visible).click();
+//
+//        Assert.assertTrue($(By.xpath("//h1[text()='Sauce Labs']")).is(Condition.visible));
+//
+//        WebDriver webDriver = WebDriverRunner.getWebDriver(); // extract web driver in selenide
+//
+//        Set<String> windowHandles = webDriver.getWindowHandles();
+//
+//        Assert.assertEquals(windowHandles.size(), 2);
+//
+//        Selenide.closeWindow();
+//
+//        Selenide.switchTo().window(0);
+//
+//        mainPage().getMainPageItems().get(2).click();
+//
+//        detailItemPage().waitDetailPageImg();
+//
+//        System.out.println(detailItemPage().getDetailItemName() + " - " + detailItemPage().getDetailItemPrice());
 
 
 
